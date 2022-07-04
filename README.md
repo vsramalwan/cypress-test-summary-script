6a9095bc5db7af42518e301e1df169a997ca4ce0
Vinod <vsramalwan@gmail.com>
2022-07-04 11:46:28 +0200

    fix: api

---
+++ README.md
@@ -0,0 +1,11 @@
+1. `npm run build`
+2. `node --experimental-json-modules dist/index.js`
+
+
+
+Sample output:
+
+| Duration | Total Tests | State |
+| -------- | ----------- | ----- |
+| Z | 6665 | failed |
+| Z | 6665 | failed |
--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-import {markdownTable} from 'markdown-table'
+import { markdownTable } from "markdown-table";
 import result from "./result.json";
 
-result?.results[0]?.suites[0]?.suites[0]?.tests?.forEach(test => {
-    console.log(markdownTable([
-        ['Duration', 'Total Tests'],
-        [test?.title, test?.duration?.toString()]
-    ]))
+console.log(markdownTable([["Duration", "Total Tests", "State"]]));
+result?.results[0]?.suites[0]?.suites[0]?.tests?.forEach((test) => {
+  console.log(
+    markdownTable([[test?.title, test?.duration?.toString(), test?.state]])
+      .replace("| - | ---- | ------ |", "") // need to adapt if adding more columns
+      .trim()
+  );
 });
